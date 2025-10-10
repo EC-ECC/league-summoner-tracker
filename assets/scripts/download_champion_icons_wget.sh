35270db68927807be976d28cef9464e0e5368df6
Michael <michaelkwan1238@gmail.com>
2025-10-10 18:52:40 -0400

    Create and execute wget scripts for champion and summoner spell icons and data

--- assets/scripts/download_champion_icons_wget.sh
+++ assets/scripts/download_champion_icons_wget.sh
@@ -3,6 +3,7 @@
 
 # Make sure files get downloaded to correct location, regardless of execution location
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SOURCE="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/"
 
 # -r Recursively download
 # -np no parent (do not follow links to parent dir)
@@ -10,4 +11,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # -P prefix directory (put downloaded files here)
 # -N do not re-retrieve files unless newer than local
 # -R reject files following pattern "index.html*"
-wget -r -np -nd -P "${SCRIPT_DIR}/../champions/data/" -N -R "index.html*" https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champions/
+wget -r -np -nd -P "${SCRIPT_DIR}/../champions/icons/" -N -R "index.html*" ${SOURCE}
---
+++ assets/scripts/download_summoner_spell_data_wget.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Script to download all League of Legends champion information from CommunityDragon (requires wget)
+
+# Make sure files get downloaded to correct location, regardless of execution location
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SOURCE="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/summoner-spells.json"
+
+# -r Recursively download
+# -np no parent (do not follow links to parent dir)
+# -nd no directory (do not put files in their raw/latest/.../1.json directory)
+# -P prefix directory (put downloaded files here)
+# -N do not re-retrieve files unless newer than local
+# -R reject files following pattern "index.html*"
+wget -r -np -nd -P "${SCRIPT_DIR}/../summoner-spells/data/" -N -R "index.html*" ${SOURCE}
